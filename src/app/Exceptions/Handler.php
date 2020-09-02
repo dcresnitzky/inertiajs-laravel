e21c4851124c2e2523d9f46748db2239e1b0dbd0
Daniel Resnitzky <dcresnitzky@gmail.com>
2020-09-02 18:45:32 -0300

    add sentry

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -29,13 +29,16 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Throwable  $exception
+     * @param \Throwable $exception
      * @return void
-     *
-     * @throws \Exception
+     * @throws Throwable
      */
-    public function report(Throwable $exception)
+    public function report(\Throwable $exception)
     {
+        if (app()->bound('sentry') && $this->shouldReport($exception)){
+            app('sentry')->captureException($exception);
+        }
+
         parent::report($exception);
     }
 
